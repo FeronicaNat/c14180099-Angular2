529bf4ab4cbdf50f97b4f27c784f2ede77f97848
FeronicaNat <c14180099@john.petra.ac.id>
2021-03-04 14:59:23 +0700

    2 files modified

--- src/halaman1/halaman1.component.ts
+++ src/halaman1/halaman1.component.ts
@@ -44,7 +44,6 @@ export class Halaman1Component implements OnInit {
     //  console.log(this.listnotes[0]);
      this.lennote=this.listnotes.length;
      this.globalvar.setnotes(this.listnotes);
-     this.globalvar.getnotes();
      
      for (var i = 0; i < this.lennote; i++) {
       console.log(
